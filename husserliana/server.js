431195e8116765662d123a630d3091fb1a8fac5e
plutta <existenz.ty@gmail.com>
2020-02-03 21:01:23 +0300

    Husserliana

--- husserliana/server.js
+++ husserliana/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
+
 
 app.use(express.static(__dirname + '/dist/husserliana'));
 
 app.get('/*', function(req, res) {
-  res.send(path.join(__dirname + '/dist/husserliana/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/husserliana/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
