246937f4a303c65a85dcfe1421e7080142166fb9
plutta <existenz.ty@gmail.com>
2020-02-08 01:32:57 +0300

    Heroku Deploy

--- husserliana/server.js
+++ husserliana/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/kek'));
+app.use(express.static(__dirname + '/dist/husserliana'));
 
 app.get('/*', function(req, res) {
-  res.send(path.join(__dirname + '/dist/kek/index.html'));
+  res.send(path.join(__dirname + '/dist/husserliana/index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
