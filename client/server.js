ca4a1d2c8ad902b9f0c76f9ae839c4208b296736
plutta <existenz.ty@gmail.com>
2020-05-02 16:51:00 +0300

    deploying to heroku

--- client/server.js
+++ client/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/client/'));
+app.use(express.static(__dirname + 'dist/client/'));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/index.html'));
 });
