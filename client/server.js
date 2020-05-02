c4d376bcb086ce644467352fbad3901341b951be
plutta <existenz.ty@gmail.com>
2020-05-02 14:32:59 +0300

    GH Pages

--- client/server.js
+++ client/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
-const ngApp = express();
-ngApp.use(express.static('./dist/angular-forms-validation'));
-ngApp.get('/*', function (request, response) {
-    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
+const compression = require('compression');
+
+const app = express();
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, 'dist/husserliana/client')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/husserliana/client/index.html'));
 });
-ngApp.listen(process.env.PORT || 8080);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => console.log(`server started ${port}`));
