7cddda4e399547cf741def6cf6a4ce261eeb0776
plutta <existenz.ty@gmail.com>
2020-05-02 17:06:06 +0300

    deploying to heroku

--- client/server.js
+++ client/server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + 'dist/client/'));
+app.use(express.static(__dirname + 'dist/husserliana/client/'));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/client/index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
